a37f15fcd3f97cada0a3a78bcc97b41307088f7e
MomchilDeus <momchil.kolev@protonmail.com>
2018-05-23 01:28:18 +0300

    Post API Routes

---
+++ models/Post.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  /* 
+   * Because we want posts and comments to remain behind, after a user has deleted his account,
+   * perhaps they are helpful,
+   * we add name and avatar separately
+   */
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  // like post = add user_id to array
+  // dislike post = remove user_id from array
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
